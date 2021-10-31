08ee10c2a5ba663a53ba172cce89b1593a595341
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-10-31 13:42:51 +0200

    refact: SPItemFieldWrapperDateTime - simplified and refactored all methods

--- SPItemFieldWrapper/SPItemFieldWrapperDateTime.cs
+++ SPItemFieldWrapper/SPItemFieldWrapperDateTime.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.SharePoint;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -10,79 +8,67 @@ namespace SPItemFieldHelpers
 {
     public class SPItemFieldWrapperDateTime : SPItemFieldWrapper
     {
+        private readonly bool _dateOnlyFormat;
+        private readonly bool _beforeRawDateTimeObjectInUniversalTime;
         public SPItemFieldWrapperDateTime(SPListItem listItem, string fieldTitle, SPItemEventProperties properties) : 
             base(listItem, fieldTitle, properties)
         {
+            SPFieldDateTime fieldDateTime = (SPFieldDateTime)Item.ParentList.Fields.GetField(Title);
+            _dateOnlyFormat = fieldDateTime.DisplayFormat == SPDateTimeFieldFormatType.DateOnly;
+            _beforeRawDateTimeObjectInUniversalTime = IsBeforeValueInUniversalTime();
         }
-
         protected override string GetValueBeforeForCompare()
         {
-            dynamic valueBeforeRawConverted = ConvertBeforeValueToLocalTimeByConditions();
-            if (String.IsNullOrEmpty(valueBeforeRawConverted?.ToString()))
+            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
                 return String.Empty;
-            if (Item.ParentList.BaseTemplate == SPListTemplateType.Events ||
-                Item.ParentList.BaseTemplate == SPListTemplateType.TasksWithTimelineAndHierarchy
-            )
-                return valueBeforeRawConverted.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            if (_beforeRawDateTimeObjectInUniversalTime)
+                return ValueBeforeRaw.ToString("yyyy-MM-ddTHH:mm:ssZ");
             else
-                return valueBeforeRawConverted.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+                return ValueBeforeRaw.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
         }
         protected override string GetValueAfterForCompare()
         {
             if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
                 return String.Empty;
-            if (ValueAfterRaw != null && ValueAfterRaw.GetType().Name != "DateTime")
-                return ValueAfterRaw.ToString();
-            else
+            if (ValueAfterRaw.GetType() == typeof(DateTime))
                 return ValueAfterRaw.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
-        }
-        //TODO: refactor to appropriate particular code
-        public override string GetValueAfterFriendly()
-        {
-            return GetFriendlyFieldValue(ValueAfterRaw);
+            else
+                return ValueAfterRaw.ToString();
         }
         public override string GetValueBeforeFriendly()
         {
-            return GetFriendlyFieldValue(ValueBeforeRaw);
-        }
-        private string GetFriendlyFieldValue(dynamic fieldValue)
-        {
-            dynamic fieldDateTime = Item.ParentList.Fields.GetField(Title);
-            string friendlyFieldValue;
-            string fieldValueAsString = fieldValue?.ToString();
-            if (String.IsNullOrEmpty(fieldValueAsString))
+            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
                 return String.Empty;
-
-            if (
-                Item.ParentList.BaseTemplate == SPListTemplateType.Events ||
-                Item.ParentList.BaseTemplate == SPListTemplateType.TasksWithTimelineAndHierarchy
-            )
-                friendlyFieldValue = DateTime.Parse(fieldValue.ToString().Replace("Z", "")).ToString();
+            string valueBeforeFriendly;
+            if (_beforeRawDateTimeObjectInUniversalTime)
+                valueBeforeFriendly = ValueBeforeRaw.ToLocalTime().ToString();
             else
-                friendlyFieldValue =
-                    fieldValueAsString.Contains("Z") ?
-                    DateTime.Parse(fieldValueAsString).ToLocalTime().ToString() : 
-                    DateTime.Parse(fieldValueAsString).ToString();
-            if (fieldDateTime.DisplayFormat.ToString() == "DateOnly")
-                friendlyFieldValue = Regex.Replace(friendlyFieldValue, @"\s[\d:]+$", "");
-            return friendlyFieldValue;
+                valueBeforeFriendly = ValueBeforeRaw.ToString();
+            if (_dateOnlyFormat)
+                valueBeforeFriendly = Regex.Replace(valueBeforeFriendly, @"\s[\d:]+$", String.Empty);
+            return valueBeforeFriendly;
         }
-        private dynamic ConvertBeforeValueToLocalTimeByConditions()
+        public override string GetValueAfterFriendly()
         {
-            dynamic fieldDateTime = Item.ParentList.Fields.GetField(Title);
-            dynamic valueToLocalTimeByConditions = ValueBeforeRaw;
-            if (
-                 fieldDateTime.DisplayFormat.ToString() == "DateOnly" &&
-                 ValueAfterRaw != null &&
-                 Regex.IsMatch(ValueAfterRaw.ToString(), @"T00:00:00Z$") &&
-                 Item.ParentList.BaseTemplate != SPListTemplateType.Events &&
-                 Item.ParentList.BaseTemplate != SPListTemplateType.TasksWithTimelineAndHierarchy
-             )
-                valueToLocalTimeByConditions =
-                    (ValueBeforeRaw != null && ValueBeforeRaw.ToString() != String.Empty) ?
-                    ValueBeforeRaw.ToLocalTime() :
-                    null;
-            return valueToLocalTimeByConditions;
+            if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
+                return String.Empty;
+            string valueAfterFriendly;
+            if (ValueAfterRaw.GetType() == typeof(DateTime))
+                valueAfterFriendly = ValueAfterRaw.ToString();
+            else
+                valueAfterFriendly = DateTime.Parse(ValueAfterRaw.ToString()).ToString();
+            if (_dateOnlyFormat)
+                valueAfterFriendly = Regex.Replace(valueAfterFriendly, @"\s[\d:]+$", String.Empty);
+            return valueAfterFriendly;
+        }
+        private bool IsBeforeValueInUniversalTime()
+        {
+            bool fieldValueInUniversalTime = Item.ParentList.BaseTemplate != SPListTemplateType.Events &&
+                Item.ParentList.BaseTemplate != SPListTemplateType.TasksWithTimelineAndHierarchy &&
+                _dateOnlyFormat &&
+                ValueAfterRaw != null &&
+                Regex.IsMatch(ValueAfterRaw.ToString(), @"T00:00:00Z$");
+            return fieldValueInUniversalTime;
         }
     }
 }
