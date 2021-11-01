c36ca02cf6699c11ea9a28d449356ac98004de3d
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-01 10:46:44 +0200

    feat: SPItemFieldWrapperDateTime - added field _afterValueInUniversalTime and inserted check of it to methods.

--- SPItemFieldWrapper/SPItemFieldWrapperDateTime.cs
+++ SPItemFieldWrapper/SPItemFieldWrapperDateTime.cs
@@ -9,54 +9,68 @@ namespace SPItemFieldHelpers
     public class SPItemFieldWrapperDateTime : SPItemFieldWrapper
     {
         private readonly bool _dateOnlyFormat;
-        private readonly bool _beforeRawDateTimeObjectInUniversalTime;
+        private readonly bool _beforeValueInUniversalTime;
+        private readonly bool _afterValueInUniversalTime;
         public SPItemFieldWrapperDateTime(SPListItem listItem, string fieldTitle, SPItemEventProperties properties) : 
             base(listItem, fieldTitle, properties)
         {
             SPFieldDateTime fieldDateTime = (SPFieldDateTime)Item.ParentList.Fields.GetField(Title);
             _dateOnlyFormat = fieldDateTime.DisplayFormat == SPDateTimeFieldFormatType.DateOnly;
-            _beforeRawDateTimeObjectInUniversalTime = IsBeforeValueInUniversalTime();
+            _beforeValueInUniversalTime = IsBeforeValueInUniversalTime();
+            _afterValueInUniversalTime = IsAfterValueInUniversalTime();
         }
         protected override string GetValueBeforeForCompare()
         {
+            string valueForCompare;
             if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
-                return String.Empty;
-            if (_beforeRawDateTimeObjectInUniversalTime)
-                return ValueBeforeRaw.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                valueForCompare = String.Empty;
+            else if (_beforeValueInUniversalTime)
+                valueForCompare = ValueBeforeRaw.ToString("yyyy-MM-ddTHH:mm:ssZ");
             else
-                return ValueBeforeRaw.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+                valueForCompare = ValueBeforeRaw.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+            return valueForCompare;
         }
         protected override string GetValueAfterForCompare()
         {
+            string valueForCompare;
             if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
-                return String.Empty;
-            if (ValueAfterRaw.GetType() == typeof(DateTime))
-                return ValueAfterRaw.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+                valueForCompare = String.Empty;
+            else if (_afterValueInUniversalTime)
+                valueForCompare = DateTime
+                    .Parse(ValueAfterRaw.ToString())
+                    .ToUniversalTime()
+                    .ToString("yyyy-MM-ddTHH:mm:ssZ");
             else
-                return ValueAfterRaw.ToString();
+                valueForCompare = DateTime
+                    .Parse(Regex.Replace(ValueAfterRaw.ToString(), "Z$", String.Empty))
+                    .ToUniversalTime()
+                    .ToString("yyyy-MM-ddTHH:mm:ssZ");
+            return valueForCompare;
         }
         public override string GetValueBeforeFriendly()
         {
-            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
-                return String.Empty;
             string valueBeforeFriendly;
-            if (_beforeRawDateTimeObjectInUniversalTime)
+            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
+                valueBeforeFriendly = String.Empty;
+            else if (_beforeValueInUniversalTime)
                 valueBeforeFriendly = ValueBeforeRaw.ToLocalTime().ToString();
             else
                 valueBeforeFriendly = ValueBeforeRaw.ToString();
+
             if (_dateOnlyFormat)
                 valueBeforeFriendly = Regex.Replace(valueBeforeFriendly, @"\s[\d:]+$", String.Empty);
             return valueBeforeFriendly;
         }
         public override string GetValueAfterFriendly()
         {
-            if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
-                return String.Empty;
             string valueAfterFriendly;
-            if (ValueAfterRaw.GetType() == typeof(DateTime))
-                valueAfterFriendly = ValueAfterRaw.ToString();
-            else
+            if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
+                valueAfterFriendly = String.Empty;
+            else if (_afterValueInUniversalTime)
                 valueAfterFriendly = DateTime.Parse(ValueAfterRaw.ToString()).ToString();
+            else
+                valueAfterFriendly = DateTime.Parse(Regex.Replace(ValueAfterRaw.ToString(), "Z$", String.Empty)).ToString();
+
             if (_dateOnlyFormat)
                 valueAfterFriendly = Regex.Replace(valueAfterFriendly, @"\s[\d:]+$", String.Empty);
             return valueAfterFriendly;
@@ -68,6 +82,14 @@ namespace SPItemFieldHelpers
                 _dateOnlyFormat &&
                 ValueAfterRaw != null &&
                 Regex.IsMatch(ValueAfterRaw.ToString(), @"T00:00:00Z$");
+            return fieldValueInUniversalTime;
+        }
+        private bool IsAfterValueInUniversalTime()
+        {
+            bool fieldValueInUniversalTime = Item.ParentList.BaseTemplate != SPListTemplateType.Events &&
+                Item.ParentList.BaseTemplate != SPListTemplateType.TasksWithTimelineAndHierarchy &&
+                ValueAfterRaw != null &&
+                ValueAfterRaw?.GetType() != typeof(DateTime);
             return fieldValueInUniversalTime;
         }
     }
