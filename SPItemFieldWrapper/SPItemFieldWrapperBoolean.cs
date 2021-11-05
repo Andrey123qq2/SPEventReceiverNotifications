70f86278a5b07353b87bbc776a41e6085f9353a6
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-05 17:27:49 +0200

    feat: added class SPItemFieldWrapperBoolean

---
+++ SPItemFieldWrapper/SPItemFieldWrapperBoolean.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPItemFieldHelpers
+{
+    class SPItemFieldWrapperBoolean : SPItemFieldWrapper
+    {
+        public SPItemFieldWrapperBoolean(SPListItem listItem, string fieldTitle, SPItemEventProperties properties) :
+            base(listItem, fieldTitle, properties)
+        {
+        }
+        protected override string GetValueBeforeForCompare()
+        {
+            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
+                return String.Empty;
+            else
+                return ValueBeforeRaw.ToString();
+        }
+        protected override string GetValueAfterForCompare()
+        {
+            if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
+                return String.Empty;
+            else
+                return ValueAfterRaw.ToString();
+        }
+        public override string GetValueAfterFriendly()
+        {
+            return ValueAfterRaw.ToString();
+        }
+        public override string GetValueBeforeFriendly()
+        {
+            return ValueBeforeRaw.ToString();
+        }
+    }
+}
