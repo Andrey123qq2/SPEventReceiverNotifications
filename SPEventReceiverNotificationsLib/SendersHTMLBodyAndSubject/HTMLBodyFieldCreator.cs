f223a5d32992e651ee537304f6c0722e7f043063
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-12 18:15:33 +0200

    feat: HTMLBodyFieldCreator - added prop Constant, added method IsShowAlwaysAndNotChanged, changed ReplaceAllMacrosInFieldTemplate with using of method IsShowAlwaysAndNotChanged

--- SPEventReceiverNotificationsLib/SendersHTMLBodyAndSubject/HTMLBodyFieldCreator.cs
+++ SPEventReceiverNotificationsLib/SendersHTMLBodyAndSubject/HTMLBodyFieldCreator.cs
@@ -1,22 +1,22 @@
-﻿using System;
+﻿using SPCustomHelpers;
+using SPItemFieldHelpers;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using SPCustomHelpers;
-using SPItemFieldHelpers;
 
 namespace SPEventReceiverNotificationsLib.SendersHTMLBodyAndSubject
 {
     class HTMLBodyFieldCreator
     {
         public readonly bool ShowAlways;
+        public readonly bool Constant;
         public readonly bool IsChanged;
         private readonly ERItemContext<List<ConfigItem>, ConfigItemGlobal> _context;
         private readonly string _fieldTemplate;
         private readonly Regex _fieldIntNameRegex = new Regex(@"(?<=(data-intname=""))\w+");
-        private readonly Regex _fieldConstantRegex = new Regex(@"data-showalways=""true""");
+        private readonly Regex _fieldShowAlwaysRegex = new Regex(@"data-showalways=""true""");
+        private readonly Regex _fieldConstantRegex = new Regex(@"data-constant=""true""");
         private readonly string _commentedFieldTemplate = "<!-- {0} -->";
         private readonly string _fieldIntName;
         private readonly SPItemFieldWrapper _fieldWrapper;
@@ -27,7 +27,8 @@ namespace SPEventReceiverNotificationsLib.SendersHTMLBodyAndSubject
             _context = context;
             _fieldTemplate = fieldTemplate;
             _fieldIntName = _fieldIntNameRegex.Match(fieldTemplate).ToString();
-            ShowAlways = _fieldConstantRegex.IsMatch(fieldTemplate);
+            ShowAlways = _fieldShowAlwaysRegex.IsMatch(fieldTemplate);
+            Constant = _fieldConstantRegex.IsMatch(fieldTemplate);
             _fieldWrapper = _context.EventTypeAsync ? 
                 SPItemFieldWrapperFactory.Create(_context.CurrentItem, _fieldIntName, null) :
                 SPItemFieldWrapperFactory.Create(_context.CurrentItem, _fieldIntName, _context.EventProperties);
@@ -37,7 +38,7 @@ namespace SPEventReceiverNotificationsLib.SendersHTMLBodyAndSubject
         }
         public string CreateHTMLBodyField()
         {
-            string filledFieldTemplate = (!ShowAlways && !IsChanged) ?
+            string filledFieldTemplate = (!Constant && !ShowAlways && !IsChanged) ?
                 String.Format(_commentedFieldTemplate, _fieldTemplate) :
                 ReplaceAllMacrosInFieldTemplate();
             return filledFieldTemplate;
@@ -47,14 +48,22 @@ namespace SPEventReceiverNotificationsLib.SendersHTMLBodyAndSubject
             StringBuilder fieldTemplateBuilder = new StringBuilder(_fieldTemplate);
             StringBuilder convertedString = fieldTemplateBuilder
                 .Replace("{NAME}", _fieldWrapper.DisplayName)
-                .Replace("{PREVVALUE}", _valueBeforeFriendly)
-                .Replace("{NEWVALUE}", _valueAfterFriendly);
+                .Replace("{PREVVALUE}", IsShowAlwaysAndNotChanged() ? String.Empty : _valueBeforeFriendly)
+                .Replace("{NEWVALUE}", IsShowAlwaysAndNotChanged() ? _valueBeforeFriendly : _valueAfterFriendly);
             return convertedString.ToString();
         }
         private bool CheckChangeConditions()
         {
-            bool checkForCommentCondition = _fieldTemplate.Contains("{PREVVALUE}") || !String.IsNullOrEmpty(_valueAfterFriendly);
-            return checkForCommentCondition && _fieldWrapper.ValueIsChanged();
+            bool checkForChangeConditions = 
+                (
+                    _fieldTemplate.Contains("{PREVVALUE}") || 
+                    !String.IsNullOrEmpty(_valueAfterFriendly)
+                ) && _fieldWrapper.ValueIsChanged();
+            return checkForChangeConditions;
+        }
+        private bool IsShowAlwaysAndNotChanged()
+        {
+            return (ShowAlways || Constant) && !_fieldWrapper.ValueIsChanged();
         }
     }
 }
