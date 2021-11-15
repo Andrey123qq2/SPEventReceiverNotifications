64243ad42681adf0eeb3dbd5a4d49e56a1c8ed4a
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-15 09:08:55 +0200

    feat: added filter class ChangedBySvcAccountsFilter

---
+++ SPEventReceiverNotificationsLib/ConfFilters/ChangedBySvcAccountsFilter.cs
@@ -0,0 +1,27 @@
+﻿using SPEventReceiverNotificationsLib.Infrastructure;
+using System.Text.RegularExpressions;
+using ERItemContextAlias = SPCustomHelpers.ERItemContext
+    <
+        System.Collections.Generic.List<SPEventReceiverNotificationsLib.ConfigItem>,
+        SPEventReceiverNotificationsLib.ConfigItemGlobal
+    >;
+
+namespace SPEventReceiverNotificationsLib.ConfFilters
+{
+    public class ChangedBySvcAccountsFilter : IConfFilter
+    {
+        private readonly ConfigItem _conf;
+        private readonly ERItemContextAlias _context;
+        public ChangedBySvcAccountsFilter(ConfigItem conf, ERItemContextAlias context)
+        {
+            _conf = conf;
+            _context = context;
+        }
+        public bool Passed()
+        {
+            //TODO: get regexp grom global conf and get disabling from local conf
+            string accountMatch = @"app@sharepoint|svc_";
+            return !Regex.IsMatch(_context.EventProperties.UserDisplayName, accountMatch);
+        }
+    }
+}
