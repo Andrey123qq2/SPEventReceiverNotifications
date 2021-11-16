e74e8845af9ae663a67a41bbf50cdc5ceb245a30
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-16 10:02:42 +0200

    feat: SPCustomHelpers - added class SPListExtensions

---
+++ SPCustomHelpers/SPCustomExtensions/SPListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SharePoint;
+
+namespace SPCustomHelpers.SPCustomExtensions
+{
+    public static class SPListExtensions
+    {
+        public static SPListItemCollection QueryItems(this SPList list, string queryFitlerString)
+        {
+            SPQuery spQuery = new SPQuery
+            {
+                Query = queryFitlerString
+            };
+            SPListItemCollection items = list.GetItems(spQuery);
+            return items;
+        }
+    }
+}
