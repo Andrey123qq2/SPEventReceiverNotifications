8a4eecf91478576bc4f0c1b17046aeef4dc08b61
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-16 10:03:03 +0200

    feat: SPCustomHelpers - added class SPWebApplicationExtensions

---
+++ SPCustomHelpers/SPCustomExtensions/SPWebApplicationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SPCustomHelpers.SPCustomExtensions
+{
+    public static class SPWebApplicationExtensions
+    {
+        public static List<SPSite> GetSitesWithFeature(this SPWebApplication webApp, string featureName)
+        {
+            var sites = webApp.Sites
+                .Where(s =>
+                {
+                    var feature = s.Features.FirstOrDefault(f => f.Definition.DisplayName == featureName);
+                    return feature != null;
+                }).ToList();
+            return sites;
+        }
+    }
+
+}
