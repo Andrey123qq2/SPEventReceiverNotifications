c5382a23df7e13d74e5fb03b6a410366992a7010
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-16 10:03:48 +0200

    feat: SPCustomHelpers - added class SPListContext

---
+++ SPCustomHelpers/SPListContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.SharePoint;
+using SPCustomHelpers.SPCustomExtensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SPCustomHelpers
+{
+    public class SPListContext<T>
+    {
+        public SPList CurrentList { get; }
+        public T ListConf { get; }
+        public bool DisableUpdatePermissions = false;
+        private ISPListContextStrategy<T> _modifierStrategy;
+        public SPListContext(SPList list, string confPopertyName = "")
+        {
+            CurrentList = list;
+            ListConf = PropertyBagConfHelper<T>.Get(list.RootFolder.Properties, confPopertyName);
+        }
+        public void SetStrategy(ISPListContextStrategy<T> strategy)
+        {
+            _modifierStrategy = strategy;
+        }
+        public void ExecuteStrategy()
+        {
+            if (_modifierStrategy == null)
+                return;
+            _modifierStrategy.Execute(this);
+        }
+        public static List<SPListContext<T>> Factory(SPSite site, string confPopertyName)
+        {
+            List<SPListContext<T>> listsToChange = site.GetListsWithJSONConf(confPopertyName)
+                .Select(l => new SPListContext<T>(l, confPopertyName))
+                .ToList();
+            return listsToChange;
+        }
+    }
+}
