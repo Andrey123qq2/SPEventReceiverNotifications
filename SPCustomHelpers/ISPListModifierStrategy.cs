35c5d0f517e75c9e2f709ecd260fa6346735f932
Nesterov Andrey <andrey.nesterov@deps.ua>
2021-11-16 10:03:29 +0200

    feat: SPCustomHelpers - added class ISPListContextStrategy

---
+++ SPCustomHelpers/ISPListModifierStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPCustomHelpers
+{
+    public interface ISPListContextStrategy<T>
+    {
+        void Execute(SPListContext<T> context);
+    }
+
+}
