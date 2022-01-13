07dd851281e47467e1fe663af70a3f744479b271
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-13 16:39:12 +0200

    feat: CommonConstants - added SETTINGS_BODY_FIELD_TEMPLATE_ADDTYPE, SETTINGS_BODY_FIELD_TEMPLATE_UPDATINGTYPE

--- SPEventReceiverNotificationsLib/CommonConstants.cs
+++ SPEventReceiverNotificationsLib/CommonConstants.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SPEventReceiverNotificationsLib
+﻿namespace SPEventReceiverNotificationsLib
 {
     public class CommonConstants
     {
         public static readonly string LIST_PROPERTY_JSON_CONF = "er_splistsnotifications_json_conf";
         public static readonly string SITE_PROPERTY_JSON_CONF = "er_splistsnotificationsglobal_json_conf";
         public static readonly string ERROR_MESSAGE_TEMPLATE = "ER custom SPListsNotifications Exception: {0}, {1} " + "[ {2} ].";
+        public static readonly string SETTINGS_BODY_FIELD_TEMPLATE_ADDTYPE = "\t\t<!-- {0} -->\r\n\t\t<p><span data-intname=\"{1}\" data-showalways=\"false\" >{{NAME}}: {{NEWVALUE}}</span></p>\r\n";
+        public static readonly string SETTINGS_BODY_FIELD_TEMPLATE_UPDATINGTYPE = "\t\t<!-- {0} -->\r\n\t\t<p><span data-intname=\"{1}\" data-showalways=\"false\" >{{NAME}}: <s>{{PREVVALUE}}</s> {{NEWVALUE}}</span></p>\r\n";
     }
 }
