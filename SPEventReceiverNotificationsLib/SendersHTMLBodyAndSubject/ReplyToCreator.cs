183eeadfcadacb2699a548d5ae12f27d6e4c8bbe
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-13 16:37:23 +0200

    feat: added class ReplyToCreator

---
+++ SPEventReceiverNotificationsLib/SendersHTMLBodyAndSubject/ReplyToCreator.cs
@@ -0,0 +1,29 @@
+﻿using SPCustomHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SPEventReceiverNotificationsLib.SendersHTMLBodyAndSubject
+{
+    class ReplyToCreator
+    {
+        protected ERItemContext<List<ConfigItem>, ConfigItemGlobal> _context;
+        private readonly string _replytoTemplate;
+        public ReplyToCreator(string replytoTemplate, ERItemContext<List<ConfigItem>, ConfigItemGlobal> context)
+        {
+            _context = context;
+            _replytoTemplate = replytoTemplate;
+        }
+        public string GetReplyto()
+        {
+            StringBuilder replyToBuilder = new StringBuilder(_replytoTemplate);
+            replyToBuilder
+                .Replace("{ITEMID}", _context.CurrentItem.ID.ToString())
+                .Replace("{LISTGUIDBASE64}", Convert.ToBase64String(_context.CurrentItem.ParentList.ID.ToByteArray()).Replace("=", String.Empty));
+            return replyToBuilder.ToString();
+        }
+    }
+}
