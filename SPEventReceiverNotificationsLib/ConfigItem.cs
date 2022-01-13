c5350f30c80d38056844d813be72552341658040
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-13 16:41:15 +0200

    feat: ConfigItem - added props SingleMode, ExcludedManagersMails, ReplyToTemplate

--- SPEventReceiverNotificationsLib/ConfigItem.cs
+++ SPEventReceiverNotificationsLib/ConfigItem.cs
@@ -14,11 +14,14 @@ namespace SPEventReceiverNotificationsLib
         public string Notes { get; set; }
         public string SendType { get; set; }
         public string EventType { get; set; }
+        public bool SingleMode { get; set; }
         public List<string> ContentTypeFilter { get; set; } = new List<string>();
         public bool DisableGlobalAccountExclusion { get; set; }
         public List<string> ToMails { get; set; } = new List<string>();
         public List<string> CCMails { get; set; } = new List<string>();
         public List<string> BCCMails { get; set; } = new List<string>();
+        public List<string> ExcludedManagersMails { get; set; } = new List<string>();
+        public string ReplyToTemplate { get; set; }
         public string SubjectTemplate { get; set; }
         public string BodyTemplate { get; set; }
         public List<string> ToFields { get; set; } = new List<string>();
