7283ef02a4923ef329364dc18f4ca24e5a577033
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-13 16:39:38 +0200

    feat: added class SPItemFieldWrapperFilteredLookup

---
+++ SPItemFieldWrapper/SPItemFieldWrapperFilterLookup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.SharePoint;
+using System;
+
+namespace SPItemFieldHelpers
+{
+    class SPItemFieldWrapperFilteredLookup : SPItemFieldWrapper
+    {
+        public SPItemFieldWrapperFilteredLookup(SPListItem listItem, string fieldTitle, SPItemEventProperties properties) :
+            base(listItem, fieldTitle, properties)
+        {
+        }
+        protected override string GetValueBeforeForCompare()
+        {
+            if (String.IsNullOrEmpty(ValueBeforeRaw?.ToString()))
+                return String.Empty;
+            else
+                return ((SPFieldLookupValue)ValueBeforeRaw).LookupId.ToString();
+        }
+        protected override string GetValueAfterForCompare()
+        {
+            if (String.IsNullOrEmpty(ValueAfterRaw?.ToString()))
+                return String.Empty;
+            else
+                return ((SPFieldLookupValue)ValueAfterRaw).LookupId.ToString();
+        }
+        public override string GetValueAfterFriendly()
+        {
+            return ((SPFieldLookupValue)ValueBeforeRaw).LookupValue;
+        }
+        public override string GetValueBeforeFriendly()
+        {
+            return ((SPFieldLookupValue)ValueAfterRaw).LookupValue;
+        }
+    }
+}
