f985fef3789c15234c61030b90ad2c569b5fdf65
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-13 16:33:45 +0200

    feat: ISenderCreator - added method CreateSenderMulti

--- SPEventReceiverNotificationsLib/Infrastructure/ISenderCreator.cs
+++ SPEventReceiverNotificationsLib/Infrastructure/ISenderCreator.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using SPCustomHelpers;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SPEventReceiverNotificationsLib.Infrastructure
 {
     public interface ISenderCreator
     {
         ISender CreateSender();
+        List<ISender> CreateSenderMulti();
     }
 }
