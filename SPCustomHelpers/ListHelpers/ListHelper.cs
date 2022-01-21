a28f2666629cc17eee92c95d6f863e3482a60b56
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-01-21 02:28:39 +0200

    feat: added helper class ListHelper

---
+++ SPCustomHelpers/ListHelpers/ListHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+
+namespace SPCustomHelpers.ListHelpers
+{
+    public static class ListHelper
+    {
+        public static DataTable ToDataTable<T>(this List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                var type = (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>) ? Nullable.GetUnderlyingType(prop.PropertyType) : prop.PropertyType);
+                dataTable.Columns.Add(prop.Name, type);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
