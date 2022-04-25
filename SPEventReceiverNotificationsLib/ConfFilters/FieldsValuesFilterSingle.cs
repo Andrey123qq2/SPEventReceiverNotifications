0eb7efe113aa4a999425c2aaf858369cc70534cf
Nesterov Andrey <andrey.nesterov@deps.ua>
2022-04-25 11:25:38 +0300

    feat: added class FieldsValuesFilterSingle

---
+++ SPEventReceiverNotificationsLib/ConfFilters/FieldsValuesFilterSingle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPEventReceiverNotificationsLib.ConfFilters
+{
+    public class FieldsValuesFilterSingle
+    {
+        public string FieldName;
+        public string Value;
+        public bool AndMode;
+    }
+}
